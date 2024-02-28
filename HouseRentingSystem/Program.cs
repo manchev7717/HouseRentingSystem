546e173c0a0e348034ba619d8eedbfcb8064f9e0
Nikolay <115391462+manchev7717@users.noreply.github.com>
2024-02-28 15:54:36 +0200

    Add project files.

---
+++ HouseRentingSystem/Program.cs
@@ -0,0 +1,37 @@
+using HouseRentingSystem.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplicationDbContex(builder.Configuration);
+builder.Services.AddApplicationIdentity(builder.Configuration);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddApplicationServices();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapDefaultControllerRoute();
+app.MapRazorPages();
+
+app.Run();
